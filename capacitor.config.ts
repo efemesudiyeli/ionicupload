f96b4dca695b10d9b872b175ec10f648c9ec62e2
efemesudiyeli <efemesudiyeli@gmail.com>
2022-04-25 18:37:50 +0300

    upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.efemesudiyeli.ionicupload',
+  appName: 'ionicrImage',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
